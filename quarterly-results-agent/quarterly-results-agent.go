2c9d0c4eb764779fa4ba9d9b99b89ce76affa7a7
oliverbull <ojbull1@gmail.com>
2025-01-15 18:03:18 +0000

    update for document query - not yet working

--- quarterly-results-agent/quarterly-results-agent.go
+++ quarterly-results-agent/quarterly-results-agent.go
@@ -44,13 +44,95 @@ var quarterlyResultsTools = &genai.Tool{
 	}},
 }
 
+// month quarters (1 is Jan, etc...)
+var q1 = []string{"1", "2", "3"}
+var q2 = []string{"4", "5", "6"}
+var q3 = []string{"7", "8", "9"}
+var q4 = []string{"10", "11", "12"}
+
 // tool to check if a quarterly result is available
 func getResults(ticker string, year string, quarter string) string {
 	log.Println("running getResults tool for " + ticker + " for " + quarter + " - " + year)
 
-	// ToDo: get the results
+	// first check if the ticker directory exists
+	resultsRoot, ok := os.LookupEnv("RESULTS_DATA")
+	if !ok {
+		log.Println("environment variable RESULTS_DATA not set")
+	}
+	if _, err := os.Stat(resultsRoot + ticker); os.IsNotExist(err) {
+		// does not exist, so reply
+		return "quarterly results for " + ticker + " are not available."
+	}
 
-	return "todo"
+	// now check if the quarter and year exists
+	filepath := ""
+	switch quarter {
+	case "q-1":
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q1[0] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q1[0] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q1[1] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q1[1] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q1[2] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q1[2] + "-quarterly-results.html"
+			break
+		}
+	case "q-2":
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q2[0] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q2[0] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q2[1] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q2[1] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q2[2] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q2[2] + "-quarterly-results.html"
+			break
+		}
+	case "q-3":
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q3[0] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q3[0] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q3[1] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q3[1] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q3[2] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q3[2] + "-quarterly-results.html"
+			break
+		}
+	case "q-4":
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q4[0] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q4[0] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q4[1] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q4[1] + "-quarterly-results.html"
+			break
+		}
+		if _, err := os.Stat(resultsRoot + ticker + "/" + year + "-" + q4[2] + "-quarterly-results.html"); !os.IsNotExist(err) {
+			filepath = resultsRoot + ticker + "/" + year + "-" + q4[2] + "-quarterly-results.html"
+			break
+		}
+	default:
+		return "unhandled quarter format: " + quarter
+	}
+	if filepath == "" {
+		return "quarterly results not found."
+	}
+
+	// read the file and return the contents
+	resultsDat, err := os.ReadFile(filepath)
+	if err != nil {
+		log.Println("failed to read results file")
+		return "failed to retrieve quarterly results."
+	}
+	return string(resultsDat)
 }
 
 // agent initialization
